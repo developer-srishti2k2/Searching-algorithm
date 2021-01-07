bc1ea56ce71a1bf31da49a3a11ef697e3f3f18e1
Srishti Tripathi <76778050+developer-srishti2k2@users.noreply.github.com>
2021-01-07 14:15:53 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-    int i,a[10],n,b=1;
+    int i,n,a[10],max,min,mid;
     printf("Enter an array of ten elements\n");
     for(i=0;i<=9;i++)
     {
@@ -9,17 +9,31 @@ void main()
     }
     printf("Enter any number\n");
     scanf("%d",&n);
-    for(i=0;i<=9;i++)
+    max=9;
+    min=0;
+    mid=(max+min)/2;
+    while(max>=min)
     {
-        if(a[i]==n)
+
+        if(a[mid]==n)
         {
-            i=i+1;
-            b=0;
-            printf("The element is found at position %d\n",i);
+            printf("The number is found at position %d",mid+1);
+            break;
+        }
+        else if(a[mid]>n)
+        {
+            max=mid-1;
+            mid=(max+min)/2;
+        }
+        else
+        {
+            min=mid+1;
+            mid=(max+min)/2;
         }
     }
-    if(b==1)
-    {
-        printf("the element is not found\n");
-    }
+        if(max<min)
+        {
+            printf("The number is not found\n");
+        }
+
 }
